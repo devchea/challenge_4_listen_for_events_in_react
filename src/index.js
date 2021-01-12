019454869e7bbf558ca4c2a02c2cce082b0dc750
devchea <34663914+devchea@users.noreply.github.com>
2021-01-12 08:50:22 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ScotchInfoBar from './ScotchInfoBar';
-import './styles.css';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
 
 function App() {
+  const [password, setPassword] = useState([]);
+
+  const handleChange = (e) => {
+    setPassword(e);
+  };
+
+  useEffect(() => {
+    if (password === "open sesame") {
+      alert("You may enter");
+      setPassword("");
+    }
+  }, [password]);
+
   return (
     <div className="App">
       <h2>What's the secret phrase?</h2>
 
-      <input type="text" placeholder="Super duper secret" />
+      <input
+        type="text"
+        placeholder="Super duper secret"
+        onChange={(e) => handleChange(e.target.value)}
+      />
 
       <p>
         Hint: It's <strong>open sesame</strong>
@@ -19,5 +36,5 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
